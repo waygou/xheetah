cfd097eb3bbfc202f769052e9a7913a86273e437
Waygou <bruno.falcao@waygou.com>
2018-11-27 18:15:58 +0100

    wip

---
+++ src/Mail/Client/NewClientAdminUser.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Waygou\Xheetah\Mail\Client;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class NewClientAdminUser extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('vendor.xheetah.emails.client.new_client_admin_user');
+    }
+}
